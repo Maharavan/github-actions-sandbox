60cc10447bb0211b35d953f6b67fe27017226ad7
Maharavan <maharavan10@gmail.com>
2025-08-23 13:58:48 +0530

    go command fixed

--- third-action-env-go-demo/main.go
+++ third-action-env-go-demo/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	fmt.Println("Connecting Crypto API to get details of all supported coins")
 	apiKey, found := os.LookupEnv("CRYPTO_KEY")
+	fmt.Println(apiKey)
 	url := fmt.Sprintf("https://api.coingecko.com/api/v3/coins/list?x_cg_demo_api_key=%s", apiKey)
 	if found {
 		resp, err := http.Get(url)
@@ -16,7 +17,7 @@ func main() {
 			fmt.Printf("Failed due to %s", err)
 		}
 		defer resp.Body.Close()
-		fmt.Println("Result is",resp)
+		fmt.Println("Result is", resp)
 
 	} else {
 		fmt.Println("CRYTPO_KEY not set")
