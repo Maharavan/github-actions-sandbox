fad1fd8be463c7dae48af9330847a0b12567aa0e
Maharavan <maharavan10@gmail.com>
2025-08-25 20:36:26 +0530

    upload to aws s3

--- .github/actions/deploy-s3-javascript/deploy.js
+++ .github/actions/deploy-s3-javascript/deploy.js
@@ -1,9 +1,19 @@
 const core =  require('@actions/core')
-const github = require('@actions/exec')
+// const github = require('@actions/github') // provide access to github rest api
 const exec = require('@actions/exec')
 
 function run(){
+    const bucket = core.getInput('buckets',{ required: true});
+    const bucket_region = core.getInput('buckets-region',{required: true});
+    const deploy = core.getInput('dist-folder',{required:true});
+
+    github.
+
     core.notice('Hello from deploy to AWS S3');
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${deploy} ${s3Uri} --region ${bucket_region} `)
+
+
 }
 
 run();
