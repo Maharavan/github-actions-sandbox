bfb60d6958cdef419fa2c582b59167be13221816
Maharavan <maharavan10@gmail.com>
2025-08-25 21:26:05 +0530

    use custom action docker

--- .github/actions/deploy-s3-javascript/deploy.js
+++ .github/actions/deploy-s3-javascript/deploy.js
@@ -12,7 +12,7 @@ function run(){
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${deploy} ${s3Uri} --region ${bucket_region} `)
     
-    const websiteUrl= `http://${bucket}.s3-website-${bucket_region}.amazonaws.com`
+    const websiteUrl= `http://${bucket}.s3-website.${bucket_region}.amazonaws.com`
     core.setOutput('website-url',websiteUrl)
 
 }
